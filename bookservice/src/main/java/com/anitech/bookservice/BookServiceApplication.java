2e913707390ba28fc403dc0e60e316e819f77253
Tapas Jena <tapas.friends@gmail.com>
2018-10-06 16:31:45 +0800

    Java service configured to use JDK 11

--- bookservice/src/main/java/com/anitech/bookservice/BookServiceApplication.java
+++ bookservice/src/main/java/com/anitech/bookservice/BookServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
+
 }
