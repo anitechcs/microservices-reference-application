617e7ece7999537f5284e6ebdfc9ae44630bdaaa
chanduthedev <chanduthedev@gmail.com>
2021-01-15 08:05:34 +0800

    promotion service logic and test cases added

--- promotion-service/promotionservice/test/test_promotion_service_controller.py
+++ promotion-service/promotionservice/test/test_promotion_service_controller.py
@@ -17,9 +17,8 @@ class TestPromotionServiceController(BaseTestCase):
     def test_get_book_promotions(self):
         """Test case for get_book_promotions
 
-        
         """
-        headers = { 
+        headers = {
             'Accept': 'application/json',
         }
         response = self.client.open(
@@ -28,6 +27,40 @@ class TestPromotionServiceController(BaseTestCase):
             headers=headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_book_promotions_for_offer_data(self):
+        """Test case for get_book_promotions response validation for valid book id
+        """
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/v1/promotion/{book_id}'.format(book_id=56),
+            method='GET',
+            headers=headers)
+
+        data = json.loads(response.data)
+        expected_color_codes = ["#CD2626", "#FF7F0F", "#FF9CEE",
+                                "#6EB5FF", "#B10501"]
+        expected_discount = [10, 15, 20, 25, 30]
+        self.assertEqual(200, data['statusCode'])
+        self.assertEqual("Success", data['message'])
+        self.assertTrue(data['promotions']['color'] in expected_color_codes)
+        self.assertTrue(int(data['promotions']['text'][:2]) in expected_discount)
+
+    def test_get_book_promotions_for_invalid_book_id(self):
+        """Test case for get_book_promotions response validation for invalid book
+        """
+        headers = {
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/v1/promotion/{book_id}'.format(book_id=0),
+            method='GET',
+            headers=headers)
+
+        data = json.loads(response.data)
+        self.assertEqual(404, data['statusCode'])
 
 
 if __name__ == '__main__':
