18644db40c01baf9c4b8d15a95b59dc1cb375b6a
chanduthedev <chanduthedev@gmail.com>
2021-01-15 08:05:34 +0800

    promotion service logic and test cases added

--- promotion-service/promotionservice/controllers/promotion_service_controller.py
+++ promotion-service/promotionservice/controllers/promotion_service_controller.py
@@ -3,7 +3,14 @@ import six
 
 from promotionservice.models.promotion_error_response import PromotionErrorResponse  # noqa: E501
 from promotionservice.models.promotion_success_response import PromotionSuccessResponse  # noqa: E501
+from promotionservice.models.promotion import Promotion  # noqa: E501
 from promotionservice import util
+from random import randint
+from flask_api import status
+
+
+# from constants import COLOR_CODE_LIST, OFFER_LIST
+from promotionservice import constants
 
 
 def get_book_promotions(book_id):  # noqa: E501
@@ -16,4 +23,21 @@ def get_book_promotions(book_id):  # noqa: E501
 
     :rtype: PromotionSuccessResponse
     """
-    return 'do some magic!'
+
+    if not book_id:
+        return PromotionErrorResponse(status.HTTP_404_NOT_FOUND,
+                                      "Book Id cannot be zero.")
+
+    # Random value between 0 and 4 (both includes)
+    offer_index = randint(0, constants.DATA_LIST_SIZE-1)
+
+    # Offer % basing on offer_index
+    offer_text = f"{constants.OFFER_LIST[offer_index]}% off"
+
+    # Offer color code basing on offer_index
+    color_code = constants.COLOR_CODE_LIST[offer_index]
+
+    promotion = Promotion(offer_text, color_code)
+
+    return PromotionSuccessResponse(status.HTTP_200_OK,
+                                    "Success", promotion)
